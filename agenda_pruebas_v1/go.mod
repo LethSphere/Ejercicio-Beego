29248fac611e434ff0aadabb3f0cc53b5b86b07b
LethSphere <lethsphere@gmail.com>
2023-12-28 10:22:58 -0500

    feat
    construcción api beego

---
+++ agenda_pruebas_v1/go.mod
@@ -0,0 +1,7 @@
+
+module agenda_pruebas_v1
+
+go 1.21
+
+require github.com/beego/beego/v2 v2.1.0
+require github.com/smartystreets/goconvey v1.6.4
---
+++ agenda_pruebas_v1/routers/router.go
@@ -0,0 +1,68 @@
+// @APIVersion 1.0.0
+// @Title beego Test API
+// @Description beego has a very cool tools to autogenerate documents for your API
+// @Contact astaxie@gmail.com
+// @TermsOfServiceUrl http://beego.me/
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+package routers
+
+import (
+	"github.com/udistrital/Ejercicio-Beego/agenda_pruebas_v1/controllers"
+
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+	ns := beego.NewNamespace("/v1",
+
+		beego.NSNamespace("/area_tipo",
+			beego.NSInclude(
+				&controllers.AreaTipoController{},
+			),
+		),
+
+		beego.NSNamespace("/tipo_parametro",
+			beego.NSInclude(
+				&controllers.TipoParametroController{},
+			),
+		),
+
+		beego.NSNamespace("/parametro",
+			beego.NSInclude(
+				&controllers.ParametroController{},
+			),
+		),
+
+		beego.NSNamespace("/parametro_periodo",
+			beego.NSInclude(
+				&controllers.ParametroPeriodoController{},
+			),
+		),
+
+		beego.NSNamespace("/periodo",
+			beego.NSInclude(
+				&controllers.PeriodoController{},
+			),
+		),
+
+		beego.NSNamespace("/contacto",
+			beego.NSInclude(
+				&controllers.ContactoController{},
+			),
+		),
+
+		beego.NSNamespace("/telefono",
+			beego.NSInclude(
+				&controllers.TelefonoController{},
+			),
+		),
+
+		beego.NSNamespace("/correo",
+			beego.NSInclude(
+				&controllers.CorreoController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
+}
